c3f775f2dda5d9463d172e49f278812b8df67da7
rksgr <r_kjha@yahoo.com>
2021-12-21 07:03:29 +0530

    Added: Welcome Message

---
+++ MyNodeMain.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.dec_20_linkedlist;
+
+
+public class MyNodeMain {
+
+    
+    public static void main(String []args){
+        System.out.println("Welcome to BridgeLabz!");
+        System.out.println("Today we shall perform basic operations"
+                + "related to linked list.");
+    }
+}
